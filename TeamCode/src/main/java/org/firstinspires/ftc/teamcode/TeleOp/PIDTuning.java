d67f7950500eda3bb7e87fb38ff022704e8f6f16
awesumdude42 <sammyiyers@gmail.com>
2025-01-20 16:14:43 -0800

    PID stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/PIDTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/PIDTuning.java
@@ -4,8 +4,11 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "PID")
 public class PIDTuning extends LinearOpMode {
 
 
