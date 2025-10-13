e1f4b7014e78f160a3eefbf110b5cfed90352ca4
awesumdude42 <sammyiyers@gmail.com>
2025-10-13 16:36:11 -0700

    drive + intake created test to manage intake better

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/test/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/test/ServoTest.java
@@ -1,7 +1,9 @@
-package org.firstinspires.ftc.teamcode.util.mercurial;
+package org.firstinspires.ftc.teamcode.util.test;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.util.mercurial.ServoSubsystem;
 
 import dev.frozenmilk.mercurial.Mercurial;
 import dev.frozenmilk.mercurial.bindings.BoundGamepad;
@@ -14,12 +16,11 @@ public class ServoTest extends OpMode {
     public void init() {
         BoundGamepad Gamepad2 = Mercurial.gamepad2();
 
-        Gamepad2.rightBumper().onTrue (ServoSubsystem.MoveServo1(0));
+        Gamepad2.rightBumper().onTrue(ServoSubsystem.MoveServo1(0));
         Gamepad2.leftBumper().onTrue(ServoSubsystem.MoveServo1(1));
-    }
 
-    @Override
-    public void loop() {
+    }
+    public void loop(){
 
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/test/intakeAndRoller.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.util.test;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.util.mercurial.drivetrainSubsystem;
+import org.firstinspires.ftc.teamcode.util.mercurial.intakeRollerSubsystem;
+
+import dev.frozenmilk.mercurial.Mercurial;
+import dev.frozenmilk.mercurial.bindings.BoundGamepad;
+import dev.frozenmilk.mercurial.commands.groups.Advancing;
+
+
+@Mercurial.Attach
+@intakeRollerSubsystem.Attach
+@drivetrainSubsystem.Attach
+@TeleOp(name = "intake + Drive")
+public class intakeAndRoller extends OpMode {
+
+    @Override
+    public void init() {
+        BoundGamepad gamepad2 = Mercurial.gamepad2();
+        gamepad2.leftBumper().onTrue(new Advancing(intakeRollerSubsystem.SpinIntake(),intakeRollerSubsystem.StopIntake()));
+    }
+
+
+
+    @Override
+    public void loop() {
+
+    }
+}
