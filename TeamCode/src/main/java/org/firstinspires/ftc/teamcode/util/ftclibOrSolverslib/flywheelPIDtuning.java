d601fc3eeea8ba3ab44b1176f3faef8c621d987f
awesumdude42 <sammyiyers@gmail.com>
2025-10-14 19:08:24 -0700

    Interpolated look up table added to flywheel tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ftclibOrSolverslib/flywheelPIDtuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ftclibOrSolverslib/flywheelPIDtuning.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.util.ftclib;
+package org.firstinspires.ftc.teamcode.util.ftclibOrSolverslib;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -6,9 +6,12 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.seattlesolvers.solverslib.controller.PIDController;
 import com.seattlesolvers.solverslib.controller.PIDFController;
 import com.seattlesolvers.solverslib.controller.wpilibcontroller.SimpleMotorFeedforward;
+import com.seattlesolvers.solverslib.util.InterpLUT;
+
 @Config
 public class flywheelPIDtuning extends OpMode {
-
+    InterpLUT dist =new InterpLUT();
+    InterpLUT vel = new InterpLUT();
     PIDFController Feedback;
     SimpleMotorFeedforward feedforward;
     PIDController teest;
@@ -17,21 +20,24 @@ public class flywheelPIDtuning extends OpMode {
     static double kD = 0;
     static double kS = 0;
     static double kV = 0;
+    double length;// detected by camera
     double f = 0;// placholder cus in the loop it changes the f to the feedforward output
     DcMotorEx flyWheel;
     @Override
     public void init() {
 
+
     feedforward = new SimpleMotorFeedforward(kS,kV);
     Feedback = new PIDFController(kP,kI,kD,f);
     flyWheel = hardwareMap.get(DcMotorEx.class,"flywheel");
+    distLUTinit();
     }
 
     @Override
     public void loop() {
         Feedback.setPIDF(kP,kI,kD,f);
-        double distance;// gonna use camera for this so ill update this later
-        double velocity = 0;// gonna be based on distance or smt
+        double distance = dist.get(length);// gonna use camera for this so ill update this later
+        double velocity = vel.get(distance);// gonna be based on distance or smt
         Feedback.setF(feedforward.calculate(flyWheel.getVelocity(),velocity));
         double power = Feedback.calculate(flyWheel.getVelocity(),velocity);
 
@@ -40,4 +46,19 @@ public class flywheelPIDtuning extends OpMode {
         flyWheel.setPower(power);
 
     }
+    public void distLUTinit(){
+        double pixelLength =0;//placeholders
+        double distance = 0;
+        dist.add(pixelLength,distance);// prob not gonna be a variable,
+        // and also have mutliple add statements
+        dist.createLUT();
+    }
+    public void velocityLUTinit(){
+        double distance = 0;// placeholders
+        double velocity = 0;
+        vel.add(distance,velocity);// same thing as above function
+        vel.createLUT();
+
+    }
+
 }
