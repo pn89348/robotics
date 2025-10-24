e0f4d22aabd3a5c91b26520154f769cac2a07d10
awesumdude42 <sammyiyers@gmail.com>
2025-10-23 19:19:32 -0700

    added kicker subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/mercurial/KickerSubsystem.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode.util.mercurial;
+
+import androidx.annotation.NonNull;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import dev.frozenmilk.dairy.core.dependency.Dependency;
+import dev.frozenmilk.dairy.core.dependency.annotation.SingleAnnotation;
+import dev.frozenmilk.dairy.core.wrapper.Wrapper;
+import dev.frozenmilk.mercurial.commands.Lambda;
+import dev.frozenmilk.mercurial.commands.util.Wait;
+import dev.frozenmilk.mercurial.subsystems.Subsystem;
+import kotlin.annotation.MustBeDocumented;
+
+public class KickerSubsystem implements Subsystem {
+    //ALWAYS CHANGE THIS TO THE RIGHT CLASS
+    public static final KickerSubsystem INSTANCE = new KickerSubsystem();
+    public static CRServo s;
+    private KickerSubsystem() {
+    }
+
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @MustBeDocumented
+    @Inherited
+    public @interface Attach{}
+
+    private Dependency<?> dependency =
+
+            Subsystem.DEFAULT_DEPENDENCY
+//ALWAYS CHANGE THIS TO THE RIGHT CLASS
+                    .and(new SingleAnnotation<>(KickerSubsystem.Attach.class));
+
+
+    @NonNull
+    @Override
+    public Dependency<?> getDependency() {
+        return dependency;
+    }
+
+    @Override
+    public void setDependency(@NonNull Dependency<?> dependency) {
+        this.dependency = dependency;
+    }
+
+    public void preUserInitHook(@NonNull Wrapper opMode) {
+        HardwareMap hmap = opMode.getOpMode().hardwareMap;
+        s = hmap.get(CRServo.class,"kicker");
+
+
+
+    }
+
+
+    //commands
+    public static Lambda kick() {
+        return new Lambda("kick artifact")
+                .setInit(() -> {
+
+                    s.setPower(1);
+                    new Wait(0.5);
+                    s.setPower(0);
+                })
+                .setExecute(() -> {
+
+                })
+                .setEnd(interrupted -> {
+
+                })
+                .setFinish(() -> {
+                    return true;
+                })
+                .setInterruptible(false)
+                .setRequirements(INSTANCE)
+                .setRunStates(Wrapper.OpModeState.ACTIVE);
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/opencv/AprilTagVelocityCalc.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.util.opencv;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class AprilTagVelocityCalc extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
